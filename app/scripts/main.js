bb36d16ab77e8abe1a1cb02de17675ea7a58fcf1
Kboris20 <klettboris@gmail.com>
2015-06-24 17:04:42 +0200

    vers6

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,8 @@
-console.log('avantReady');
-
-	$(document).ready(function(){
-		$('a').smoothScroll();
-		console.log('dansReady');
-		$( 'div.progress' ).each(function( index ) {
-		var myPieChart = new Chart(ctx[0]).Pie(data,options);	}
+$(document).ready(function(){
+	'use strict'
+		$('a').smoothScroll();		
+}
 );
-	});
-console.log ('Après READY');
+		
+
 
